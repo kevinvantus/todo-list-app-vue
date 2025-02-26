f6a060c475d5fc06703f6c8d2118673519e786db
Kevin Vantus <kevinvantus@gmail.com>
2025-02-26 23:34:23 +0100

    chore: design text input component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 import MainHeader from './components/MainHeader.vue'
+import TextInput from './components/TextInput.vue'
 </script>
 
 <template>
   <div class="container">
+    <MainHeader />
     <main>
-      <MainHeader />
+      <TextInput />
     </main>
   </div>
 </template>
---
+++ src/components/TextInput.vue
@@ -0,0 +1,45 @@
+<template>
+  <form class="text-container">
+    <div class="oval"></div>
+
+    <input type="text" placeholder="Create a new todo..." />
+  </form>
+</template>
+
+<style scoped>
+form {
+  padding-inline: 20px;
+  height: 48px;
+  box-shadow: 0px 35px 50px -15px #c2c3d680;
+  background-color: #fff;
+  border-radius: 5px;
+  display: flex;
+  align-items: center;
+  gap: 12px;
+}
+
+.oval {
+  width: 20px;
+  height: 20px;
+  flex-shrink: 0;
+  border-radius: 50%;
+  background: linear-gradient(135deg, #55ddff 0%, #c058f3 100%);
+}
+
+input {
+  width: 100%;
+  height: 100%;
+  font-size: 0.75rem;
+  border: none;
+  outline: none;
+  color: hsla(235, 14%, 26%, 1);
+  caret-color: var(--bright-blue);
+  font-family: Josefin Sans;
+  line-height: 0.75;
+  letter-spacing: -0.17px;
+}
+
+::placeholder {
+  color: hsla(235, 9%, 61%, 1);
+}
+</style>
